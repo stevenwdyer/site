1046ecf5a0a458f3627cfaad8fbab89531c71841
stevenwdyer <swdyer@mun.ca>
2026-01-02 12:04:26 -0330

    Add projects page and resume

--- main.js
+++ main.js
@@ -2,3 +2,30 @@
 function button_redirect(link){
   window.open(link, "", "");
 }
+
+// Can use to make projects collapsible in the future
+function open_project_collapsible(project_name){
+  $(".collapse").collapse('hide'); // Hide all collapsable elements
+  $(".nav-link").removeClass('active'); 
+  $("."+project_name).collapse('show'); // Show the project in interest
+  $("."+project_name+"_button").addClass('active'); 
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  // Load header and footer
+  fetch('includes/header.html')
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById('header-placeholder').innerHTML = data;
+      });
+
+  fetch('includes/footer.html')
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById('footer-placeholder').innerHTML = data;
+      });
+
+  // Scroll to the top of the page on load
+  window.scrollTo(0, 0);
+});
